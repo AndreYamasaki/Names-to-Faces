8794facdb5ec922f4e309912bbce660d9f03183b
Andre <katsuya.tsu@gmail.com>
2021-07-27 12:37:23 -0300

    - UserDefaults with Codable protocol

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -12,21 +12,75 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     //MARK: - Attributes
     
     var people = [Person]()
+    var picker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Fail to load people")
+            }
+        }
     }
     
     //MARK: - Methods
     
     @objc func addNewPerson() {
         
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallery()
+        }))
+
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+        
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
-        present(picker, animated: true)
+    }
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            picker.allowsEditing = false
+            self.present(picker, animated: true, completion: nil)
+        } else {
+            let ac = UIAlertController(title: "Error", message: "You are in simulator. This is not real Neo", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Go back", style: .cancel))
+            present(ac, animated: true)
+        }
+    }
+    
+    func openGallery() {
+        let imagePicker = UIImagePickerController()
+          imagePicker.delegate = self
+          imagePicker.allowsEditing = true
+          imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+          self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people")
+        }
+        
     }
     
     //MARK: - CollectionView DataSource
@@ -62,13 +116,21 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         ac.addAction(UIAlertAction(title: "Ok", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else {return}
             person.name = newName
+            self?.save()
             self?.collectionView.reloadData()
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
+        
+//Challenge 1: 1. Add a second UIAlertController that gets shown when the user taps a picture, asking them whether they want to rename the person or delete them.
+        ac.addAction(UIAlertAction(title: "Delete pic", style: .default) {
+            [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.reloadData()
+        })
     }
     
-    //MARK: - CollectionView Delegate
+    //MARK: - ImagePickerController Delegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
@@ -82,10 +144,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "unknown", image: imageName)
         people.append(person)
+        save()
         collectionView.reloadData()
         
         dismiss(animated: true)
     }
+    
     
     func getDocumentsDirectory() -> URL {
         
